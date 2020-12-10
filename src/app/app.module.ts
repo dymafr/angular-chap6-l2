b3cb7cecc3d7aa5af95130592c13413956bff615
dymafr <erwan1309@hotmail.com>
2020-12-10 13:40:48 +0100

    end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { ColorDirective } from "./color.directive";
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ColorDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
